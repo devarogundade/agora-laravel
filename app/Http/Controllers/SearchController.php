49ba98de09b60481b264aadba915509ff21e7073
Arogundade Ibrahim <devarogundade@gmail.com>
2022-05-22 17:20:19 +0100

    commit

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -57,4 +57,25 @@ class SearchController extends Controller
             200
         );
     }
+
+    public function getSpeechToken(Request $request) {
+        header('Access-Control-Allow-Origin: ' . $_SERVER['SERVER_NAME']);
+
+        $subscriptionKey = $request->key;
+        $region = $request->region;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://' . $region . '.api.cognitive.microsoft.com/sts/v1.0/issueToken');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, '{}');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Ocp-Apim-Subscription-Key: ' . $subscriptionKey));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        $token = curl_exec($ch);
+
+        return response()->json([
+            'status' => true,
+            'data' => $token
+        ]);
+    }
 }
