2c45d1276402a52bab2ff18d4f33e17144ea2153
Arogundade Ibrahim <devarogundade@gmail.com>
2022-05-30 15:12:05 +0100

    commit

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Asset;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 
 class SearchController extends Controller
 {
@@ -12,31 +11,31 @@ class SearchController extends Controller
     {
         $keywords = explode(" ", $request->text);
 
-        $assets = Asset::where(function ($query) use ($keywords) {
-            foreach ($keywords as $keyword) {
-                $query->orWhere('type', 'like', "%{$keyword}%");
-                $query->orWhere('state', 'like', "%{$keyword}%");
-                $query->orWhere('location', 'like', "%{$keyword}%");
-                $query->orWhere('name', 'like', "%{$keyword}%");
-                $query->orWhere('price', 'like', "% {$keyword} %");
-                $query->orWhere('metadata', 'like', "%{$keyword}%");
-                $query->orWhere('about', 'like', "%{$keyword}%");
-            }
+        $base_query = Asset::where('id', '!=', NULL);
 
-            // foreach ($keywords as $keyword) {
-            //     $query->selectRaw(function ($querySelect) use ($keyword) {
-            //         $querySelect->selectRaw('
-            //         SELECT email,
-            //         name,
-            //         Round ((Char_length(Concat(email, name)) - Char_length(REPLACE ( Concat(email,name), "first_keyword", ""))) / Char_length("first_keyword"))
-            //         + Round ((Char_length(Concat(email, name)) - Char_length(REPLACE ( Concat(email,name), "second_keyword", ""))) / Char_length("second_keyword"))  AS count
-            //  FROM   users
-            //  Having count >0
-            //  ORDER  BY count DESC;
-            //         ');
-            //     });
-            // }
-        })->get();
+        foreach ($keywords as $keyword) {
+            $base_query->where(function ($query) use ($keyword) {
+                $query->where('type', 'like', '%' . $keyword . '%')
+                    ->orWhere('state', 'like', '%' . $keyword . '%')
+                    ->orWhere('location', 'like', '%' . $keyword . '%')
+                    ->orWhere('name', 'like', '%' . $keyword . '%')
+                    ->orWhere('price', 'like', '%' . $keyword . '%')
+                    ->orWhere('metadata', 'like', '%' . $keyword . '%')
+                    ->orWhere('about', 'like', '%' . $keyword . '%');
+            });
+        }
+
+        foreach ($keywords as $keyword) {
+            $base_query->selectRaw(
+                '
+        Round (
+            (Char_length(Concat(location, name)) - Char_length(REPLACE ( Concat(location,name), "' . $keyword . '", ""))
+        ) / Char_length("' . $keyword . '")
+        ) AS count' .  $keyword
+            )->orderBy("count$keyword", "desc");
+        }
+
+        $assets = $base_query->get();
 
         if (!$assets) {
             return response()->json(
@@ -57,5 +56,4 @@ class SearchController extends Controller
             200
         );
     }
-
 }
